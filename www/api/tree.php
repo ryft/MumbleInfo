1a86af622b4ad85cdd1e1c640073444532027b56
James Nicholls <thejamesnicholls@gmail.com>
2014-11-02 19:43:13 +0000

    Track jstree resources, set up channel info listener

--- www/api/tree.php
+++ www/api/tree.php
@@ -14,21 +14,23 @@ function format_tree($tree) {
         array_map('format_user', $users)
     );
     return array(
-        'id'        => $channel->id,
-        'text'      => ($channel->id == '0') ? 'Ryft.uk' : $channel->name,
-        'icon'      => 'fa fa-folder',
-        'state'     => array( 'opened' => (count($children) > 0) ),
+        'id'    => $channel->id,
+        'text'  => ($channel->id == '0') ? 'Ryft.uk' : $channel->name,
+        'icon'  => 'fa fa-folder',
+        'state' => array( 'opened' => (count($children) > 0) ),
         'children'  => $children,
+        'type'  => 'channel',
     );
 }
 
 function format_user($user) {
+    // Ensure user IDs can't be numberic so they can't conflict with channels
+    // Anonymous users all have id -1 but name uniqueness is enforced by mumble
     return array(
-        // Ensure user IDs can't be numberic so they can't conflict with channels
-        // Anonymous users all have id -1 but name uniqueness is enforced by mumble
-        'id'        => 'user-' . $user->name,
-        'text'      => $user->name,
-        'icon'      => 'fa fa-user',
+        'id'    => 'user-' . $user->name,
+        'text'  => $user->name,
+        'icon'  => 'fa fa-user',
+        'type'  => 'user',
     );
 }
 
--- www/js/infoPage.js
+++ www/js/infoPage.js
@@ -1,8 +1,9 @@
-$(function () { $('#channelViewer').jstree({
-    'core': {
-        'data': { 'url': 'api/channels.php' }
-    }})
-});
-$('#channelViewer').on("changed.jstree", function (e, data) {
-      console.log(data.selected);
+$(function () {
+    $('#channelViewer')
+        .on('changed.jstree', function (e, data) {
+            // Load data via AJAX to $('#channelInfo')
+    })  .jstree({
+        'core': {
+            'data': { 'url': 'api/tree.php' }
+    }});
 });
