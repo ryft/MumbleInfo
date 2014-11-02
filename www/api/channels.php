c031bc51d83b43c01bee3993b1e339525bddc660
James Nicholls <thejamesnicholls@gmail.com>
2014-11-02 15:04:36 +0000

    Use Tree interface to populate channel viewer

--- www/api/channels.php
+++ www/api/channels.php
@@ -1,21 +1,24 @@
 <?php
 require_once 'Voip/MumbleServer.php';
 
-$mumble     = new MumbleServer();
-$server     = $mumble->getServer();
+$mumble = new MumbleServer();
+$server = $mumble->getServer();
+$tree   = $server->getTree();
 
-$channels   = $server->getChannels();
-$users      = $server->getUsers();
-
-function format_channel($channel) {
+function format_tree($tree) {
+    $channel    = $tree->c;
+    $subtrees   = $tree->children;
+    $users      = $tree->users;
+    $children   = array_merge(
+        array_map('format_tree', $subtrees),
+        array_map('format_user', $users)
+    );
     return array(
         'id'        => $channel->id,
-        'parent'    => ($channel->id == '0') ? '#' : $channel->parent,
         'text'      => ($channel->id == '0') ? 'Ryft.uk' : $channel->name,
-        'state'     => array(
-                            'opened'    => true
-                        ),
         'icon'      => 'fa fa-folder',
+        'state'     => array( 'opened' => (count($children) > 0) ),
+        'children'  => $children,
     );
 }
 
@@ -24,17 +27,11 @@ function format_user($user) {
         // Ensure user IDs can't be numberic so they can't conflict with channels
         // Anonymous users all have id -1 but name uniqueness is enforced by mumble
         'id'        => 'user-' . $user->name,
-        'parent'    => $user->channel,
         'text'      => $user->name,
         'icon'      => 'fa fa-user',
     );
 }
 
-$nodes = array_merge(
-    array_map('format_user', $users),
-    array_map('format_channel', $channels)
-);
-
 header('Content-Type: application/json');
-echo json_encode($nodes);
+echo json_encode(format_tree($tree));
 ?>
