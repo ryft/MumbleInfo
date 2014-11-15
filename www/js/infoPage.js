42b4b5462bbc55f1707286d6349815b906a4f4ab
James Nicholls <thejamesnicholls@gmail.com>
2014-11-15 15:03:52 +0000

    Enumerate server details array for easier filtering

--- www/js/infoPage.js
+++ www/js/infoPage.js
@@ -1,9 +1,25 @@
 var app = angular.module('MumbleInfo', ['ngSanitize', 'infoFilters']);
 
 app.controller('detailsController', function($scope, $filter, $http) {
+    var detailKeys = [
+        { name: 'version',          title: 'Version'    },
+        { name: 'uptime',           title: 'Uptime',    filter: 'interval' },
+        { name: 'last_activity',    title: 'Last activity', filter: 'date' },
+    ];
+
     $http.get('/api/details.php')
         .success(function(response) {
-            $scope.server = response;
+            $scope.available = response.available;
+            $scope.details   = [];
+
+            angular.forEach(detailKeys, function(detail) {
+                var val = response[detail.name];
+
+                this.push({
+                    title: detail.title,
+                    value: (val) ? $filter(detail.filter || 'identity')(val) : '',
+                });
+            }, $scope.details);
         }
     );
 });
@@ -13,18 +29,18 @@ app.controller('treeController', function($scope, $filter, $http) {
         return input.slice(-4).join('.');
     };
     var userKeys = [
-        { name: 'name',         title: 'User name' },
+        { name: 'name',         title: 'User name'      },
         { name: 'release',      title: 'Client version' },
         { name: 'os',           title: 'Operating system' },
-        { name: 'osversion',    title: 'OS version' },
+        { name: 'osversion',    title: 'OS version'     },
         { name: 'onlinesecs',   title: 'Online time',   filter: 'interval' },
         { name: 'idlesecs',     title: 'Last activity', filter: 'ago' },
         { name: 'address',      title: 'IP address',    filter: 'ipv4' },
     ];
     var channelKeys = [
-        { name: 'name',         title: 'Channel name' },
-        { name: 'id',           title: 'Channel ID' },
-        { name: 'description',  title: 'Description' },
+        { name: 'name',         title: 'Channel name'   },
+        { name: 'id',           title: 'Channel ID'     },
+        { name: 'description',  title: 'Description'    },
     ];
 
     $scope.summary = 'Select a user or channel for details';
@@ -41,15 +57,12 @@ app.controller('treeController', function($scope, $filter, $http) {
                         // Select data keys depending on node type
                         var keys = (node.type == 'user') ? userKeys : channelKeys;
                         angular.forEach(keys, function(item) {
-
-                            // If a filter is defined, apply it now
                             var val = response[item.name];
 
                             this.push({
                                 title: item.title,
                                 value: $filter(item.filter || 'identity')(val),
                             });
-
                         }, $scope.info);
                     }
                 );
